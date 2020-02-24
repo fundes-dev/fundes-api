f9dd88f3ffb614d312b56e372e38c14fd22b91ce
Eugene Michasiw <eugene@michasiw.com>
2020-02-23 20:04:05 -0500

    fix mongoDB connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  // try {
-  //   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true });
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
 
-  //   console.log('MongoDB Connected...');
-  // } catch (err) {
-  //   console.error(err.message);
-  //   //Exit processs with failure
-  //   process.exit(1);
-  // }
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    //Exit processs with failure
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
